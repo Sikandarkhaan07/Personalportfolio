c767a49afd248fbb2d910a73389641d50696abab
sikandarkhaan07 <sikandarkhaan074@gmail>
2020-12-01 23:58:29 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,93 @@
+window.sr = ScrollReveal();
+// sr.reveal('#nav', {
+//   duration: 500,
+//   origin: 'left',
+//   distance: '300px'
+// });
+sr.reveal('.intro-left', {
+  duration: 2000,
+  origin: 'left',
+  distance: '400px'
+});
+sr.reveal('.intro-right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '400px'
+});
+sr.reveal('#about-content-left', {
+  duration: 2000,
+  origin: 'left',
+  distance: '400px'
+});
+sr.reveal('.about-content-right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '400px'
+});
+sr.reveal('.skills-box-left', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '400px'
+});
+sr.reveal('.skills-box-right', {
+  duration: 1000,
+  origin: 'top',
+  distance: '400px',
+  delay: 200
+});
+sr.reveal('.summary-left', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '200px',
+  delay: 200
+});
+sr.reveal('.summary-right', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '200px',
+});
+
+sr.reveal('.s1', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '200px',
+});
+sr.reveal('.s2', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '200px',
+  delay: 300
+});
+sr.reveal('.s3', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '200px',
+  delay: 150
+});
+sr.reveal('.s4', {
+  duration: 1500,
+  origin: 'bottom',
+  distance: '200px',
+  delay: 400
+});
+sr.reveal('.s5', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '200px',
+  delay: 500
+});
+sr.reveal('.s6', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '200px',
+});
+sr.reveal('.contact-left', {
+  duration: 1000,
+  origin: 'left',
+  distance: '200px',
+});
+sr.reveal('.contact-right', {
+  duration: 1000,
+  origin: 'right',
+  distance: '200px',
+});
